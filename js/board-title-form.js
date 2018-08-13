4475de0e98b3dece6a5eca502e33a606db298812
Unknown <Mbuena@student.neumont.edu>
2018-08-13 16:25:39 -0600

    Created HTML for dashboard

--- js/board-title-form.js
+++ js/board-title-form.js
@@ -1,3 +1,17 @@
 /*
 This function will build the form for adding a new board
  */
+
+function buildDashboardContainer() {
+    var node = document.createElement('div');
+    node.setAttribute("class", "boardGrid");
+    for (board in boards){
+        node.innerHTML +=
+		'<div class="boardBox">' +
+        '<i class="fas fa-pencil-alt"></i>'+
+        `<h6 class="boardName">${board.title}</h6>`
+		'</div>'
+    }
+	// node.style.display = 'none'
+	return node
+}
