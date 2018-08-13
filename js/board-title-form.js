ab069540feac3da1706ee1b55e0176a0d431d2a5
Maddy Richard <MRichard@student.neumont.edu>
2018-08-13 16:08:27 -0600

    working on js

--- js/board-title-form.js
+++ js/board-title-form.js
@@ -1,3 +1,9 @@
 /*
 This function will build the form for adding a new board
  */
+
+ function buildDashGrid(boards) {
+   var node = document.createElement("div");
+   //for each board
+   return node;
+ }
--- js/trello-main.js
+++ js/trello-main.js
@@ -2,87 +2,20 @@ var boards = [];
 
 function displayBoard(id) {
 	//use id to find correct board to display
-	'use strict'
-
-	//Board constructor object and assign some properties to its prototype
-	function Board(title) {
-		var nextId = 0
-
-		this.title = title
-		this.lists = []
-		this.cards = {}
-
-		this.node = document.createElement('div')
-		this.titleNode = document.createElement('div')
-		this.listsNode = document.createElement('div')
-
-		this.node.id = 'board'
-		this.titleNode.id = 'trello-title-board'
-		this.listsNode.id = 'trello-canvas-board'
-
-		// new list title form
-		this.titleFormNode = buildListTitleForm()
-		this.titleNode.appendChild(document.createTextNode(this.title))
-
-		this.getNextId = function () {
-			return '_' + (nextId++).toString()
-		}
-	}
-
-	Board.prototype.render = function () {
-		this.lists.push(new List(this, 'Add a list...', 0, true))
-		for (var i = 0; i < this.lists.length; ++i) {
-			this.listsNode.appendChild(this.lists[i].node)
-		}
-		this.lists[this.lists.length - 1].node.appendChild(this.titleFormNode)
-		this.lists[this.lists.length - 1].titleNode.onclick = addListTrello(this)
-		this.node.appendChild(this.titleNode)
-		this.node.appendChild(this.listsNode)
-	}
-
-	Board.prototype.registerCard = function (card, index) {
-		this.cards[card.id] = {
-			card: card,
-			list: card.list,
-			index: index
-		}
-	}
-
-	Board.prototype.reregisterSubsequent = function (list, index, shift) {
-		for (var i = index; i < list.cards.length; ++i) {
-			this.registerCard(list.cards[i], i + shift)
-		}
-	}
-
-	Board.prototype.unregisterCard = function (card) {
-		delete this.cards[card.id]
-	}
-
-
-	document.getElementById('card-edit-close').onclick = cardEdit.close
-
-	document.getElementById('card-edit-submit').onclick = cardEdit.submit
-
-	document.getElementById('card-edit-delete').onclick = cardDeleteTrello.delete
-
-	cardEdit.windowOverlay.onclick = cardEdit.close
-
-	//if you click on escape then also the edit window will get closed
-	window.onkeydown = function (evt) {
-		if (evt.keyCode === 27) {
-			cardEdit.close()
-		}
-	}
-
 
 	//Onloading the document render the board.The code starts from here
 	document.body.onload = function () {
-		var title = this.title,
-			board = new Board(title)
-
-		board.render()
-		document.getElementById('container').appendChild(board.node)
-		currentBoard = board
+		for (let i = 0; i < boards.length; i++) {
+			//get the board from boards that was clicked given the nodeid
+			if (boards[i].id == id.split("_")[1]) {
+				//re-create that board and load into html
+				var title = boards[i].title;
+				var board = new Board(title);
+				board.render();
+				document.getElementById('container').appendChild(board.node);
+				currentBoard = board;
+			}
+		}
 	}
 }
 
@@ -91,27 +24,36 @@ function displayDash(boards) {
 		this.boards = boards;
 
 		this.node = document.createElement("div");
-		//create nodes to represent html elements
-		//build form (board-title-form) for adding new boards
+		this.dashGrid = buildDashboardContainer(boards);
 		//add click event to each board to direct to board w/ board id
 	}
 
 	Dashboard.prototype.render = function () {
 		console.log(this.boards);
-		if (this.boards == null || this.boards == undefined || this.boards.length == 0) {
-			console.log("boards in undefined");
-			this.node.innerHTML = "no boards to display";
-		} else {
-			//fill div with boards
+		this.node.appendChild(this.dashGrid);
+	}
+
+	Dashboard.prototype.registerBoard = function (board, index) {
+		this.boards[board.id] = {
+			board: board,
+			index: index
 		}
 	}
+
+	Dashboard.prototype.unregisterBoard = function (board) {
+		delete this.boards[board.id]
+	}
+
+	//document.getElementById("addBoardLink").onclick = addBoard;
+	//add event listener to dynamically created button too
 
 	document.body.onload = function () {
 		//pass in list of boards
 		//var boards = [];
-		var dash = new Dashboard(boards);
-		dash.render();
+		var dashboard = new Dashboard(boards);
+		dashboard.render();
 		document.getElementById('container').appendChild(dash.node);
+		dash = dashboard;
 	}
 }
 
