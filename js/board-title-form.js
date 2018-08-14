38c65ba64f041616efd6f8466c7ad08af517cc4b
Unknown <Mbuena@student.neumont.edu>
2018-08-14 14:43:04 -0600

    Add New Board Button Tile

--- js/board-title-form.js
+++ js/board-title-form.js
@@ -5,6 +5,11 @@ This function will build the form for adding a new board
 function buildDashboardContainer() {
     var node = document.createElement('div');
     node.setAttribute("class", "boardGrid");
+    node.innerHTML +=
+        '<div class="boardBox">' +
+        '<h6 class="boardNew">Add Board</h6>' +
+        '<i class="fas fa-plus-circle"></i>' +
+        '</div>'
     for (board in boards){
         node.innerHTML +=
 		'<div class="boardBox">' +
