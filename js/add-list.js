9ba0ee257de4bf22b6ff19aaac9ec95377a6014f
Maddy Richard <MRichard@student.neumont.edu>
2018-08-14 17:31:32 -0600

    Added list/card saving; added list name editing in place

--- js/add-list.js
+++ js/add-list.js
@@ -1,20 +1,46 @@
 //This function will called on adding the list on the board
 function addListTrello(board) {
 	return function () {
-		var titleInput = document.getElementById('trello-list-title-input')
+		var titleInput = document.getElementById('trello-list-title-input');
 
-		document.getElementById('trello-list-title-submit').onclick = titleButtonClick
+		//document.getElementById('trello-list-title-submit').onclick = titleButtonClick
+		//titleInput.onclick = titleButtonClick
+
+		function addList(evt) {
+			evt.preventDefault();
+			var title = titleInput.innerHTML.trim(),
+				index = board.lists.length - 1,
+				list;
+			board.titleFormNode.style.display = 'none';
+			titleInput.value = '';
+			if (!title) {
+				return
+			}
+			if (evt.keyCode === 13) {
+				title = titleInput.innerHTML.trim();
+				title = title.toString().split("<")[0];
+				list = new List(board, title, index);
+				board.lists.splice(index, 0, list);
+				board.listsNode.insertBefore(list.node,
+					board.lists[index + 1].node)
+				board.lists[index + 1].titleNode.setAttribute('list-index', index + 1);
+			}
+		}
+
+		titleInput.addEventListener("keyup", addList);
+
 		board.titleFormNode.style.display = 'block'
 		titleInput.focus()
 
 		function titleButtonClick(evt) {
-			evt.preventDefault()
-			var title = titleInput.value.trim()
-				, index = board.lists.length - 1
-				, list
+			console.log("in click event");
+			evt.preventDefault();
+			var title = titleInput.innerHTML.trim(),
+				index = board.lists.length - 1,
+				list;
 
-			board.titleFormNode.style.display = 'none'
-			titleInput.value = ''
+			board.titleFormNode.style.display = 'none';
+			titleInput.value = '';
 			if (!title) {
 				return
 			}
--- js/trello-main.js
+++ js/trello-main.js
@@ -1,40 +1,26 @@
 var boards = new Array();
-// var testBoard = new Board("Test");
-// testBoard.title = "Test";
-// boards[0] = testBoard;
 
-
-function addNewBoard(){
-	var newBoard = new Board("Board");
-	newBoard.title = "Title 1";
+function addNewBoard() {
+	var newBoard = new Board("Title 1");
 	boards.push(newBoard);
-	console.log("BOARDS: ", boards);
+	//console.log("BOARDS: ", boards);
 	displayDash(boards);
 }
 
 
 function displayBoard() {
-	//use id to find correct board to display
 	var id = this.id;
-	console.log("BOARD CLICKED", id);
 	var boardId = id.toString().split("_");
-	console.log("split", boardId[2]);
-	console.log("BOARDS", dash.boards);
-	//Onloading the document render the board.The code starts from here
-
 		for (let i = 0; i < dash.boards.length; i++) {
 			//get the board from boards that was clicked given the nodeid
 			var boardListId = boards[i].id.toString().split("_");
-			console.log("BOARD ID", boardListId);
 			if (boardListId[1] == boardId[2]) {
-				console.log("board match");
-				//re-create that board and load into html
-				var title = boards[i].title;
-				var board = new Board(title);
-				board.render();
+				console.log("board before render", boards[i]);
+				boards[i].render(boards[i]);
+				console.log("board after render", boards[i]);
 				document.getElementById('container').innerHTML = '';
-				document.getElementById('container').appendChild(board.node);
-				currentBoard = board;
+				document.getElementById('container').appendChild(boards[i].node);
+				currentBoard = boards[i];
 			}
 		}
 	
@@ -52,24 +38,13 @@ function displayDash(boards) {
 			var boardList = document.getElementsByClassName("boards");
 			for (let i = 0; i < boardList.length; i++) {
 				boardList[i].addEventListener('click', displayBoard);
-				console.log("adding click events");
 			}
 		}
-		console.log("DASH INIT")
 		var addBoardBtn = document.getElementById('addBoardBtn');
 		addBoardBtn.addEventListener('click', addNewBoard);
 }
 
-// var boards = new Array();
-// var testBoard = new Board("Test");
-// testBoard.title = "Test";
-// boards.push(testBoard);
 displayDash(boards);
-
-
-function initDashboard(){
-	
-}
 
 function load() {
 	displayDash(boards);
